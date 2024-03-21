131bf21af42bb16d0b29206445b84db42ad18198
JacopoLombardi <lombajacopo@gmail.com>
2024-03-21 15:25:54 +0100

    [add] added Components

---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+
+
+<!-- Javascript -->
+<script>
+  export default {
+    data(){
+      return{
+        
+      }
+    },
+
+    methods:{
+
+    },
+  }
+</script>
+
+
+
+<!-- HTML -->
+<template>
+
+<h1>footer</h1>
+
+</template>
+
+
+
+<!-- CSS -->
+<style>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,32 @@
+
+
+<!-- Javascript -->
+<script>
+  export default {
+    data(){
+      return{
+        
+      }
+    },
+
+    methods:{
+
+    },
+  }
+</script>
+
+
+
+<!-- HTML -->
+<template>
+
+<h1>mainn</h1>
+
+</template>
+
+
+
+<!-- CSS -->
+<style>
+
+</style>
